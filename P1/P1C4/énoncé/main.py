55b220e213902b1e5ec081d23e47a5ce6134abcb
bloodeur1 <168866822+bloodeur1@users.noreply.github.com>
2024-05-04 22:19:12 +0200

    variable

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+nom = "kouame"
+age = 22
+print(f"je m'appelle {nom} et j'ai {age} ans.")
+print(age)
+age = age + 10 
+print(f"je m'appelle {nom} et j'ai {age} ans maintenant.")
